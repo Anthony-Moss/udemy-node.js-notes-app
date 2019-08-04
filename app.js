8d87031a4c77bca196afe37739bcacbc2efb21f8
Anthony Moss <asmoss93@gmail.com>
2019-08-03 21:28:03 -0400

    adds code for section 3 of node.js course notes app

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+const validator = require('validator');
 const getNotes = require('./notes');
+const chalk = require('chalk');
 
 const notes = getNotes()
 
-console.log(notes)
+console.log(chalk.green.bold.inverse(notes));
+console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+
+console.log(validator.isEmail('anthony@example.com'))
